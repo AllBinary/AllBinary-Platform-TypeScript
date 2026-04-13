871cda7c09f65831a939f2193d20bc3d221e072d
Travis Berthelot <travisberthelot@allbinary.com>
2026-04-13 11:26:35 -0500

    Update pom files This should be optional

--- j2me/CommonJavaLibraryTsM/src/main/java/org/allbinary/logic/TsUtil.ts
+++ j2me/CommonJavaLibraryTsM/src/main/java/org/allbinary/logic/TsUtil.ts
@@ -33,7 +33,7 @@ export class TsUtil
 
 
                         //if statement needs to be on the same line and ternary does not work the same way.
-                        return instance;
+                        return TsUtil.instance;
     
 }
 
